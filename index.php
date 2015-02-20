f687986cde31422cd49a43d8682aade3d24800d5
Jeff Hays <jhays@moguldom.com>
2015-02-20 16:55:15 -0500

    updating comment block and prepping for install hook test

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Plugin Name:	WP Modification History
+ * Plugin URI:	http://robido.com/wp-mod-history
+ * Description:	A simple plugin to enable a metabox on particular post types that shows you a history of modifications made to a post by user and time/date
+ * Version:		1.0.0
+ * Author:		Jeff Hays (jphase)
+ * Author URI:	https://profiles.wordpress.org/jphase/
+ * Text Domain:	wp-mod-history
+ * License:		GPL2
+ */
 namespace ModHistory;
 
 class Main {
